87673f810bccdbdf6b040319e940437081925b38
0John70 <kingajohn070@gmail.com>
2023-02-15 11:03:52 +0300

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,20 @@
+ 
+num1 = eval(input("Input first number: "))
+num2 = eval(input("Input second number: "))
+
+operator = input("Input operator: ")
+def add(num1 , num2):
+    result = num1 + num2
+    return result
+
+def subtract(num1 , num2):
+    result = num1 - num2
+    return result
+
+def divide(num1 , num2):
+    result = num1 / num2
+    return result    
+
+def multiply(num1 , num2):
+    result = num1 * num2
+    return result  
